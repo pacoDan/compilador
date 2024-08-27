10492c872ab5b77b448a680be02e3c5f83dd8eee
Daniel <jhonpaco@frba.utn.edu.ar>
2024-08-27 20:04:20 -0300

    :fire:

--- parser.c
+++ parser.c
@@ -1,50 +1,29 @@
 #include "parser.h"
-// typedef struct
-// {
-//     char identifi[TAMLEX];
-//     TOKEN t;
-//     /* t=0, 1, 2, 3 Palabra Reservada, t=ID=4 Identificador */
-// } RegTS;
-// RegTS TS[1000] = {{"inicio", INICIO}, {"fin", FIN}, {"leer", LEER}, {"escribir", ESCRIBIR}, {"$", 99}};
-// typedef struct
-// {
-//     TOKEN clase;
-//     char nombre[TAMLEX];
-//     int valor;
-// } REG_EXPRESION;
-// char buffer[TAMLEX];
-// TOKEN tokenActual;
-// int flagToken = 0;
-
-// ------------------
-// Aproximacion a C
+#include "scanner.h"
 /**********Procedimientos de Analisis Sintactico (PAS) *****************/
 void Objetivo(void)
 {
-    /* <objetivo>
-    -
-    > <programa> FDT #terminar */
+    /* <objetivo> -><programa> FDT #terminar */
     Programa();
     Match(FDT);
     Terminar();
 }
+
 void Programa(void)
 {
-    /* <programa>
-    -
-    > #comenzar INICIO <listaSentencias> FIN */
+    /* <programa> -> #comenzar INICIO <listaSentencias> FIN */
     Comenzar();
     Match(INICIO);
     ListaSentencias();
     Match(FIN);
 }
+
 void ListaSentencias(void)
 {
-    /* <listaSentencias>
-    -
-    > <sentencia> {<sentencia>} */
+    /* <listaSentencias> -><sentencia> {<sentencia>} */
+
     Sentencia();
-    while (1)
+    while(1)
     {
         switch (ProximoToken())
         {
@@ -58,36 +37,29 @@ void ListaSentencias(void)
         }
     }
 }
+
 void Sentencia(void)
 {
     TOKEN tok = ProximoToken();
     REG_EXPRESION izq, der;
+
     switch (tok)
     {
-    case ID:
-        /* <sentencia>
-        -
-        > ID := <expresion> #asignar ; */
+    case ID: /* <sentencia> -> ID := <expresion> #asignar ; */
         Identificador(&izq);
         Match(ASIGNACION);
         Expresion(&der);
         Asignar(izq, der);
         Match(PUNTOYCOMA);
         break;
-    case LEER:
-        /* <sentencia>
-        -
-        > LEER ( <listaIdentificadores> ) */
+    case LEER: /* <sentencia> -> LEER ( <listaIdentificadores> ) */
         Match(LEER);
         Match(PARENIZQUIERDO);
         ListaIdentificadores();
         Match(PARENDERECHO);
         Match(PUNTOYCOMA);
         break;
-    case ESCRIBIR:
-        /* <sentencia>
-        -
-        > ESCRIBIR ( <listaExpresiones> ) */
+    case ESCRIBIR: /* <sentencia> -> ESCRIBIR ( <listaExpresiones> ) */
         Match(ESCRIBIR);
         Match(PARENIZQUIERDO);
         ListaExpresiones();
@@ -98,16 +70,17 @@ void Sentencia(void)
         return;
     }
 }
+
 void ListaIdentificadores(void)
 {
-    /* <listaIdentifi
-    cadores>
-    -
-    > <identificador> #leer_id {COMA <identificador> #leer_id} */
+    /* <listaIdentificadores> -><identificador> #leer_id {COMA <identificador> #leer_id} */
+
     TOKEN t;
     REG_EXPRESION reg;
+
     Identificador(&reg);
     Leer(reg);
+
     for (t = ProximoToken(); t == COMA; t = ProximoToken())
     {
         Match(COMA);
@@ -115,23 +88,25 @@ void ListaIdentificadores(void)
         Leer(reg);
     }
 }
+
 void Identificador(REG_EXPRESION *presul)
 {
-    /* <identificador>
-    -
-    > ID #procesar_id */
+    /* <identificador> -> ID #procesar_id */
+
     Match(ID);
     *presul = ProcesarId();
 }
+
 void ListaExpresiones(void)
 {
-    /* <listaExpresiones>
-    -
-    > <expresion> #escribir_exp {COMA <expresion> #escribir_exp} */
+    /* <listaExpresiones> -><expresion> #escribir_exp {COMA <expresion> #escribir_exp} */
+
     TOKEN t;
     REG_EXPRESION reg;
+
     Expresion(&reg);
     Escribir(reg);
+
     for (t = ProximoToken(); t == COMA; t = ProximoToken())
     {
         Match(COMA);
@@ -142,13 +117,14 @@ void ListaExpresiones(void)
 
 void Expresion(REG_EXPRESION *presul)
 {
-    /* <expresion>
-    -
-    > <primaria> { <operadorAditivo> <primaria> #gen_infijo } */
+    /* <expresion> -><primaria>{ <operadorAditivo><primaria> #gen_infijo } */
+
     REG_EXPRESION operandoIzq, operandoDer;
     char op[TAMLEX];
     TOKEN t;
+
     Primaria(&operandoIzq);
+
     for (t = ProximoToken(); t == SUMA || t == RESTA; t = ProximoToken())
     {
         OperadorAditivo(op);
@@ -157,30 +133,21 @@ void Expresion(REG_EXPRESION *presul)
     }
     *presul = operandoIzq;
 }
+
 void Primaria(REG_EXPRESION *presul)
 {
     TOKEN tok = ProximoToken();
+
     switch (tok)
     {
-    case ID:
-        /* <primaria>
-        -
-        > <identificador> */
+    case ID: /* <primaria> -><identificador> */
         Identificador(presul);
         break;
-    case CONSTANTE:
-        /* <primaria>
-        -
-        > CONSTANTE #procesar_cte */
+    case CONSTANTE: /* <primaria> -> CONSTANTE #procesar_cte */
         Match(CONSTANTE);
         *presul = ProcesarCte();
         break;
-    case PARENIZQUIERDO:
-        /* <primaria>
-        -
-        > PARENIZQUIERDO <expre
-        sion>
-        PARENDERECHO */
+    case PARENIZQUIERDO: /* <primaria> -> PARENIZQUIERDO <expresion> PARENDERECHO */
         Match(PARENIZQUIERDO);
         Expresion(presul);
         Match(PARENDERECHO);
@@ -189,12 +156,13 @@ void Primaria(REG_EXPRESION *presul)
         return;
     }
 }
+
 void OperadorAditivo(char *presul)
 {
-    /* <operadorAditivo>
-    -
-    > SUMA #procesar_op | RESTA #procesar_op */
+    /* <operadorAditivo> -> SUMA #procesar_op | RESTA #procesar_op */
+
     TOKEN t = ProximoToken();
+
     if (t == SUMA || t == RESTA)
     {
         Match(t);
@@ -202,163 +170,4 @@ void OperadorAditivo(char *presul)
     }
     else
         ErrorSintactico(t);
-}
-
-/**********************Rutinas Semanticas******************************/
-REG_EXPRESION ProcesarCte(void)
-{
-    /* Convierte cadena que representa numero a numero entero y construye un registro
-    semantico */
-    REG_EXPRESION reg;
-    reg.clase = CONSTANTE;
-    strcpy(reg.nombre, buffer);
-    sscanf(buffer, "%d", &reg.valor);
-    return reg;
-}
-REG_EXPRESION ProcesarId(void)
-{
-    /*
-    Declara ID y construye el correspondiente registro semantico */
-    REG_EXPRESION reg;
-    Chequear(buffer);
-    reg.clase = ID;
-    strcpy(reg.nombre, buffer);
-    return reg;
-}
-char *ProcesarOp(void)
-{
-    /* Declara OP y construye el correspondiente registro semantico */
-    return buffer;
-}
-void Leer(REG_EXPRESION in)
-{
-    /* Genera la instruccion para leer */
-    Generar("Read", in.nombre, "Entera", "");
-}
-void Escribir(REG_EXPRESION out)
-{
-    /* Genera la inst
-    ruccion para escribir */
-    Generar("Write", Extraer(&out), "Entera", "");
-}
-REG_EXPRESION GenInfijo(REG_EXPRESION e1, char *op, REG_EXPRESION e2)
-{
-    /* Genera la instruccion para una operacion infija y construye un registro semantico con el
-    resultado */
-    REG_EXPRESION reg;
-    static unsigned int numTemp = 1;
-    char cadTemp[TAMLEX] = "Temp&";
-    char cadNum[TAMLEX];
-    char cadOp[TAMLEX];
-    if (op[0] == '-')
-        strcpy(cadOp, "Restar");
-    if (op[0] == '+')
-        strcpy(cadOp, "Sumar");
-    sprintf(cadNum, "%d", numTemp);
-    numTemp++;
-    strcat(cadTemp, cadNum);
-    if (e1.clase == ID)
-        Chequear(Extraer(&e1));
-    if (e2.clase == ID)
-        Chequear(Extraer(&e2));
-    Chequear(cadTemp);
-    Generar(cadOp, Extraer(&e1), Extraer(&e2), cadTemp);
-    strcpy(reg.nombre, cadTemp);
-    return reg;
-}
-/*********
-******Funciones Auxiliares**********************************/
-void Match(TOKEN t)
-{
-    if (!(t == ProximoToken()))
-        ErrorSintactico();
-    flagToken = 0;
-}
-TOKEN ProximoToken()
-{
-    if (!flagToken)
-    {
-        tokenActual = scanner();
-        if (tokenActual == ERRORLEXICO)
-            ErrorLexico();
-        flagToken = 1;
-        if (tokenActual == ID)
-        {
-            Buscar(buffer, TS, &tokenActual);
-        }
-    }
-    return tokenActual;
-}
-void ErrorLexico()
-{
-    printf("Error Lexico\n");
-}
-void ErrorSintactico()
-{
-    printf("Error Sintactico\n");
-}
-void Generar(char *co, char *a, char *b, char *c)
-{
-    /* Produce la salida de la instruccion para la MV por stdout */
-    printf("%s %s%c%s%c%s\n", co, a, ',', b, ',', c);
-}
-char *Extraer(REG_EXPRESION *preg)
-{
-    /* Retorn
-    a la cadena del registro semantico */
-    return preg->nombre;
-}
-int Buscar(char *id, RegTS *TS, TOKEN *t)
-{
-    /* Determina si un identificador esta en la TS */
-
-    int i = 0;
-    while (strcmp("$", TS[i].identifi))
-    {
-        if (!strcmp(id, TS[i].identifi))
-        {
-            *t = TS[i].t;
-            return 1;
-        }
-        i++;
-    }
-    return 0;
-}
-void Colocar(char *id, RegTS *TS)
-{
-    /* Agrega un identificador a la TS */
-    int i = 4;
-    while (strcmp("$", TS[i].identifi))
-        i++;
-    if (i < 999)
-    {
-        strcpy(TS[i].identifi, id);
-        TS[i].t = ID;
-        strcpy(TS[++i].identifi, "$");
-    }
-}
-void Chequear(char *s)
-{
-    /* Si la cadena No esta en la Tabla de Simbolos la agrega,
-    y si es el nombre de una variable genera la instruccion */
-    TOKEN t;
-    if (!Buscar(s, TS, &t))
-    {
-        Colocar(s, TS);
-        Generar("Declara", s, "Entera", "");
-    }
-}
-void Comenzar(void)
-{
-    /* Inicializaciones Semanticas */
-}
-void Terminar(void)
-{
-    /* Genera la instruccion para terminar la ejecucion del programa */
-    Generar("Detiene", "", "", "");
-}
-void Asignar(REG_EXPRESION izq, REG_EXPRESION der)
-{
-    /* Genera la instruccion para la asignacion */
-    Generar("Almacena", Extraer(&der), izq.nombre, "");
 }
--- scanner.c
+++ scanner.c
@@ -1,170 +1,115 @@
 #include "scanner.h"
+#include "parser.h"
 
 /**************************Scanner************************************/
 
 TOKEN scanner()
 {
     int tabla[NUMESTADOS][NUMCOLS] = {{1, 3, 5, 6, 7, 8, 9, 10, 11, 14, 13, 0, 14},
-
                                       {1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
-
                                       {14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14},
-
                                       {4, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4},
-
                                       {14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14},
-
                                       {14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14},
-
                                       {14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14},
-
                                       {14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14},
-
                                       {14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14},
-
                                       {14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14},
-
                                       {14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14},
-
                                       {14, 14, 14, 14, 14, 14, 14, 14, 14, 12, 14, 14, 14},
-
                                       {14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14},
-
                                       {14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14},
-
                                       {14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14}};
-
     int car;
     int col;
     int estado = 0;
+
     int i = 0;
+
     do
     {
         car = fgetc(in);
         col = columna(car);
         estado = tabla[estado][col];
+
         if (col != 11)
         {
             buffer[i] = car;
-
             i++;
         }
-    }
-
-    while (!estadoFinal(estado) && !(estado == 14));
+    } while (!estadoFinal(estado) && !(estado == 14));
 
     buffer[i] = '\0';
 
     switch (estado)
-
     {
-
     case 2:
         if (col != 11)
-
         {
-
             ungetc(car, in);
-
             buffer[i - 1] = '\0';
         }
-
         return ID;
-
     case 4:
         if (col != 11)
-
         {
-
             ungetc(car, in);
-
             buffer[i - 1] = '\0';
         }
-
         return CONSTANTE;
-
     case 5:
         return SUMA;
-
     case 6:
         return RESTA;
-
     case 7:
         return PARENIZQUIERDO;
-
     case 8:
         return PARENDERECHO;
-
     case 9:
         return COMA;
-
     case 10:
         return PUNTOYCOMA;
-
     case 12:
         return ASIGNACION;
-
     case 13:
         return FDT;
-
     case 14:
         return ERRORLEXICO;
     }
-
     return 0;
 }
-
 int estadoFinal(int e)
-
 {
-
     if (e == 0 || e == 1 || e == 3 || e == 11 || e == 14)
         return 0;
-
     return 1;
 }
-
 int columna(int c)
-
 {
-
     if (isalpha(c))
         return 0;
-
     if (isdigit(c))
         return 1;
-
     if (c == '+')
         return 2;
-
     if (c == '-')
         return 3;
-
     if (c == '(')
         return 4;
-
     if (c == ')')
         return 5;
-
     if (c == ',')
         return 6;
-
     if (c == ';')
         return 7;
-
     if (c == ':')
         return 8;
-
     if (c == '=')
         return 9;
-
     if (c == EOF)
         return 10;
-
     if (isspace(c))
         return 11;
-
     return 12;
 }
-
 /*************Fin Scanner**********************************************/
